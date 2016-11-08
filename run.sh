4f2653ae2787a5168b37322d56f9efa760efa9e3
Leonid Brish <leonid.v.brish@gmail.com>
2016-11-08 12:33:26 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,8 @@ else
   export WERCKER_HTTP_NOTIFY_TYPE="deploy"
 fi
 
-curl -d "result=$WERCKER_RESULT" \
-     -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
-     -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
-     -d "application_name=$WERCKER_APPLICATION_NAME" \
-     -d "git_branch=$WERCKER_GIT_BRANCH" \
-     -d "started_by=$WERCKER_STARTED_BY" \
-     -d "build_url=$WERCKER_BUILD_URL" \
-     -d "commit=$WERCKER_GIT_COMMIT" \
-     $WERCKER_HTTP_NOTIFY_URL
+curl -i \
+      -H "Accept: application/json" \
+      -H "Content-Type:application/json" \
+      -X POST --data '{"result":"'"$WERCKER_RESULT"'","type":"'"$WERCKER_HTTP_NOTIFY_TYPE"'","application_owner_name":"'"$WERCKER_APPLICATION_OWNER_NAME"'","application_name":"'"$WERCKER_APPLICATION_NAME"'","git_branch":"'"$WERCKER_GIT_BRANCH"'","started_by":"'"$WERCKER_STARTED_BY"'","build_url":"'"$WERCKER_BUILD_URL"'","commit":"'"$WERCKER_GIT_COMMIT"'"}' \
+      $WERCKER_HTTP_NOTIFY_URL
